50dbec2cc60f25136b436227af1cbdca8c640cd8
Sreejit7 <sreejitde7@gmail.com>
2022-02-25 23:40:28 +0530

    next.config update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ["media.graphcms.com"],
   },
+  env: {
+    HOST_NAME: process.env.HOST_NAME,
+    NEXT_PUBLIC_GRAPHCMS_ENDPOINT: process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
+    GRAPHCMS_AUTH_TOKEN: process.env.GRAPHCMS_AUTH_TOKEN,
+  },
 };
 
 module.exports = nextConfig;
